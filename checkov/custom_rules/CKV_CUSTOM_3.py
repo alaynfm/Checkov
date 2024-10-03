c671a8f52b920cd9e488eb6fe7b030b240424394
Alain Fernandez <47385898+alaynfm@users.noreply.github.com>
2024-10-03 11:40:20 +0200

    a

--- checkov/custom_rules/CKV_CUSTOM_3.py
+++ checkov/custom_rules/CKV_CUSTOM_3.py
@@ -1,22 +1,17 @@
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 
-class SecretManagerSecretRotation(BaseResourceCheck):
+class SecretManagerSecretExists(BaseResourceCheck):
     def __init__(self):
-        name = "Ensure Secrets Manager secrets have rotation enabled"
-        id = "CKV_CUSTOM_3"
+        name = "Ensure Secrets Manager secret is created"
+        id = "CKV_AWS_306"
         supported_resources = ("aws_secretsmanager_secret",)
         categories = (CheckCategories.SECRETS,)
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
     def scan_resource_conf(self, conf):
         if "aws_secretsmanager_secret" in conf:
-            secret_id = conf["aws_secretsmanager_secret"]["id"]
-            rotation_resources = conf.get("aws_secretsmanager_secret_rotation", [])
-            for rotation in rotation_resources:
-                if rotation.get("secret_id") == secret_id:
-                    return CheckResult.PASSED
-            return CheckResult.FAILED
-        return CheckResult.SKIP
+            return CheckResult.PASSED
+        return CheckResult.FAILED
 
-check = SecretManagerSecretRotation()
+check = SecretManagerSecretExists()
