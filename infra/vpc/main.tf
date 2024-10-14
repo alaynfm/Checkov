3e0434cc6e2912065c5d09b2d659ecb7f384d87d
alain <alaynfm@chucks.local>
2024-10-14 20:50:48 +0200

    tfsec avilable

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -56,7 +56,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = element(var.public_subnets, count.index)
   availability_zone       = element(var.availability_zones, count.index)
-  count                   = length(var.public_subnets)
+  count                   = macarroon(var.public_subnets)
   map_public_ip_on_launch = true
 
   tags = {
