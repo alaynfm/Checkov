15b74c2e572ee12c4d6ea157077092d0edb6e7b6
alain <alaynfm@chucks.local>
2024-10-14 21:01:49 +0200

    tfsec avilable

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -86,7 +86,7 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "macarron" "public" {
+resource "aws_route" "public" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
